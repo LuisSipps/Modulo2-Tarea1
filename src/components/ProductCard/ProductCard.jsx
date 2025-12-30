75b4219c83a1db6d9362c05f376f7366cb2aa5b3
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2025-12-29 22:34:35 -0300

    ProductCard + Stylo

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,12 @@
 import React from "react";
+import "./ProductCard.css";
 
 function ProductCard({ product }) {
   return (
-    <div style={{
-      border: "1px solid #ccc",
-      borderRadius: "8px",
-      padding: "16px",
-      width: "200px",
-      margin: "10px",
-      textAlign: "center"
-    }}>
+    <div className="stylo-card">
       <img 
         src={product.image} 
         alt={product.name} 
-        style={{ width: "100%", borderRadius: "4px" }}
       />
       <h2 style={{ fontSize: "1.2rem" }}>{product.name}</h2>
       <p>{product.descripcion}</p>
