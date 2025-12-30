c9a967930c845481ddcb074e8a01c18271683505
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2025-12-30 12:35:41 -0300

    Footer + Stylos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,38 @@ function Footer() {
   console.log("Footer renderizado");
 
   return (
-    <header className="footer">
-      <h1>Este es el footer</h1>
-    </header>
+    <footer className="footer">
+      <div className="footer-content">
+        <section>
+          <h4>Conocenos</h4>
+          <ul>
+            <li>Acerca de nosotros</li>
+            <li>Trabaja con nosotros</li>
+            <li>Nuestros clientes</li>
+            <li>Ayuda</li>
+          </ul>
+        </section>
+        <section>
+          <h4>Contacto</h4>
+          <ul>
+            <li>+569 82252557</li>
+            <li>chityluchy@gmail.com</li>
+          </ul>
+        </section>
+        <section>
+          <h4>Redes Sociales</h4>
+          <ul>
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>TikTok</li>
+          </ul>
+        </section>
+      </div>
+      <div className="footer-copyright">
+          <p>© 2025 Chity Serigrafía. Todos los derechos reservados.</p>
+        </div>
+    </footer>
   );
 }
 
