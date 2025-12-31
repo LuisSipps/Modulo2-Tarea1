81b927c0ecb57337307b69d66ef6ab5c4a5bb138
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2025-12-31 13:45:40 -0300

    Nuevas imagenes

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,9 +1,11 @@
+import poleraMangaCortaNegra from "./polera_manga_corta_negra.png"
 import poleraMangaLarga from "./polera_manga_larga_blanca.png";
 import poleronBlanco from "./poleron_blanco.png";
 import poleronNegro from "./poleron_negro.png";
 
 export const images = {
-  polera_manga_larga_blanca: poleraMangaLarga,
-  poleron_blanco: poleronBlanco,
-  poleron_negro: poleronNegro,
+    polera_manga_corta_negra: poleraMangaCortaNegra,
+    polera_manga_larga_blanca: poleraMangaLarga,
+    poleron_blanco: poleronBlanco,
+    poleron_negro: poleronNegro,
 };
