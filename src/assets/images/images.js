f08745788b2777e732c79d5204dbeda3e7aeb84f
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2026-01-05 21:20:39 -0300

    Se añadieron al catalogo nuevos bolsos

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,10 +1,13 @@
 import poleraMangaCortaNegra from "./polera_manga_corta_negra.png"
 import poleraMangaCortaNegraEva from "./polera_manga_corta_negra_evangelion.png";
 import poleraMangaLarga from "./polera_manga_larga_blanca.png";
+import poleraMangaLargaEva from "./polera_manga_larga_blanca_evangelion.png";
 import poleraMangaCortaBlancaEva from "./polera_manga_corta_evangelion.png";
 import poleronBlanco from "./poleron_blanco.png";
 import poleronNegro from "./poleron_negro.png";
 import logo from "./chity_serigrafia_logo.png";
+import bolsaPucca from "./bolsa_pucca.png";
+import bolsaCsm from "./bolsa_csm.png";
 
 export const images = {
     polera_manga_corta_negra: poleraMangaCortaNegra,
@@ -13,6 +16,8 @@ export const images = {
     poleron_negro: poleronNegro,
     polera_manga_corta_negra_evangelion: poleraMangaCortaNegraEva,
     polera_manga_corta_evangelion: poleraMangaCortaBlancaEva,
+    polera_manga_larga_blanca_evangelion: poleraMangaLargaEva,
     logo,
-
+    bolsa_pucca: bolsaPucca,
+    bolsa_csm: bolsaCsm,
 };
